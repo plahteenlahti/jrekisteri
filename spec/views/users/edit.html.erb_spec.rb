95cba6893d43d753a2b799452e7cb30a0d6dc5ac
Perttu <perttu.lahteenlahti@helsinki.fi>
2015-05-03 09:07:24 +0300

    Testejä

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -9,19 +9,4 @@ RSpec.describe "users/edit", type: :view do
       :city => "MyString"
     ))
   end
-
-  it "renders the edit user form" do
-    render
-
-    assert_select "form[action=?][method=?]", user_path(@user), "post" do
-
-      assert_select "input#user_forename[name=?]", "user[forename]"
-
-      assert_select "input#user_surname[name=?]", "user[surname]"
-
-      assert_select "input#user_email[name=?]", "user[email]"
-
-      assert_select "input#user_city[name=?]", "user[city]"
-    end
-  end
 end
