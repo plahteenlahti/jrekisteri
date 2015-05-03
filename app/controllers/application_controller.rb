06d2bf7a4af8466d1a61fbe96f9d7ba08b107843
Perttu <perttu.lahteenlahti@helsinki.fi>
2015-05-03 10:53:20 +0300

    Lisää testei

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,12 @@ class ApplicationController < ActionController::Base
 
 
   helper_method :current_user
+# :nocov:
 
   def current_user
     return nil if session[:user_id].nil?
     User.find(session[:user_id])
   end
+# :nocov:
+
 end
