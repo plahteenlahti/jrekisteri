25f5b7cf22e4c7af6ee6349b35a8b7810cc254a1
Perttu <perttu.lahteenlahti@helsinki.fi>
2015-05-03 13:38:08 +0300

    Membership controller tests

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -23,137 +23,37 @@ RSpec.describe MembershipsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Membership. As you add validations to Membership, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+  let!(:organization){ FactoryGirl.create(:organization) }
+  let!(:user){ FactoryGirl.create(:user) }
+  let!(:membership){FactoryGirl.create(:membership)}
+
+
+  before(:each) {
+    session[:user_id] = user.id
   }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # MembershipsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
-  describe "GET #index" do
-    it "assigns all memberships as @memberships" do
-      membership = Membership.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:memberships)).to eq([membership])
-    end
-  end
 
   describe "GET #show" do
     it "assigns the requested membership as @membership" do
-      membership = Membership.create! valid_attributes
-      get :show, {:id => membership.to_param}, valid_session
-      expect(assigns(:membership)).to eq(membership)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new membership as @membership" do
-      get :new, {}, valid_session
-      expect(assigns(:membership)).to be_a_new(Membership)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested membership as @membership" do
-      membership = Membership.create! valid_attributes
-      get :edit, {:id => membership.to_param}, valid_session
+      get :show, id: membership
       expect(assigns(:membership)).to eq(membership)
     end
   end
 
   describe "POST #create" do
-    context "with valid params" do
+
+    describe "with valid params" do
       it "creates a new Membership" do
-        expect {
-          post :create, {:membership => valid_attributes}, valid_session
+        membership_params = {"organization_id"=>"1", "user_id"=>"1"}
+        expect{
+          put :create, membership:membership_params
         }.to change(Membership, :count).by(1)
       end
 
-      it "assigns a newly created membership as @membership" do
-        post :create, {:membership => valid_attributes}, valid_session
-        expect(assigns(:membership)).to be_a(Membership)
-        expect(assigns(:membership)).to be_persisted
-      end
-
       it "redirects to the created membership" do
-        post :create, {:membership => valid_attributes}, valid_session
-        expect(response).to redirect_to(Membership.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved membership as @membership" do
-        post :create, {:membership => invalid_attributes}, valid_session
-        expect(assigns(:membership)).to be_a_new(Membership)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:membership => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
+        membership_params = {"organization_id"=>"1", "user_id"=>"1"}
+        put :create, membership:membership_params
+        expect(response).to redirect_to(Organization.first)
       end
     end
   end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested membership" do
-        membership = Membership.create! valid_attributes
-        put :update, {:id => membership.to_param, :membership => new_attributes}, valid_session
-        membership.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested membership as @membership" do
-        membership = Membership.create! valid_attributes
-        put :update, {:id => membership.to_param, :membership => valid_attributes}, valid_session
-        expect(assigns(:membership)).to eq(membership)
-      end
-
-      it "redirects to the membership" do
-        membership = Membership.create! valid_attributes
-        put :update, {:id => membership.to_param, :membership => valid_attributes}, valid_session
-        expect(response).to redirect_to(membership)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the membership as @membership" do
-        membership = Membership.create! valid_attributes
-        put :update, {:id => membership.to_param, :membership => invalid_attributes}, valid_session
-        expect(assigns(:membership)).to eq(membership)
-      end
-
-      it "re-renders the 'edit' template" do
-        membership = Membership.create! valid_attributes
-        put :update, {:id => membership.to_param, :membership => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested membership" do
-      membership = Membership.create! valid_attributes
-      expect {
-        delete :destroy, {:id => membership.to_param}, valid_session
-      }.to change(Membership, :count).by(-1)
-    end
-
-    it "redirects to the memberships list" do
-      membership = Membership.create! valid_attributes
-      delete :destroy, {:id => membership.to_param}, valid_session
-      expect(response).to redirect_to(memberships_url)
-    end
-  end
-
 end
